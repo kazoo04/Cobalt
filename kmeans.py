827303dee79553fc011911e1bdb026e72a28f228
kazoo04 <k.gokita@usagee.co.jp>
2013-05-31 20:10:06 +0900

    コードブックのサイズを増やす

--- kmeans.py
+++ kmeans.py
@@ -7,24 +7,20 @@ import re
 import struct
 import numpy
 import scipy.cluster
-from random import Random
+import random
 
-num_files = 10
+num_dimentions = 64
+num_files = 4096
+num_features_per_file = 1024
 
 def open_feature_file(filename):
-  v = []
   vectors = []
 
   with open(filename, 'rb') as f:
-    for vals in iter(lambda: f.read(64), b''):
-      for val in vals:
-        v.append(struct.unpack('<B', val)[0])
+    for vals in iter(lambda: f.read(num_dimentions), b''):
+      vectors.append([struct.unpack('<B', x)[0] for x in vals])
 
-      vectors.append(v)
-      v = []
-
-  return vectors
-
+  return random.sample(vectors, num_features_per_file)
 
 if len(sys.argv) != 2:
   print 'usage'
@@ -33,8 +29,7 @@ if len(sys.argv) != 2:
 
 files = os.listdir(sys.argv[1])
 
-rand = Random()
-rand.shuffle(files)
+random.shuffle(files)
 files = files[:num_files]
 
 vectors = []
@@ -47,7 +42,7 @@ for f in files:
 
 X = numpy.array(vectors)
 
-codebook, destortion = scipy.cluster.vq.kmeans2(X, 1e2, iter=2, thresh=1e-3)
+codebook, destortion = scipy.cluster.vq.kmeans2(X, 1e5, iter=5, thresh=1e-8)
 for row in codebook:
   print ','.join([str(r) for r in row])
 
